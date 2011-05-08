1c2d9ff083ae321029b11a85970829780935137f
xelibrion <xelibrion@gmail.com>
2011-05-08 17:19:06 +0400

    SessionManagement.csproj

---
+++ SessionManagement/SessionManagement/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace SessionManagement.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			return View();
+		}
+	}
+}
